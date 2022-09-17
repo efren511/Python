6e513078e9e00d56683f107786544d3b9556a0c7
Efren Garcia <luisefrengarciadeleon@gmail.com>
2022-09-16 23:05:22 -0500

    An example to get the data type of a variable

---
+++ Data_Type.py
@@ -0,0 +1,9 @@
+# This is a string
+Text = "Hello World..."
+
+# This is a int number
+Number = 511
+
+# We show the data type
+print("The data type of Text is: ", type(Text))
+print(" The data type of Number is: ", type(Number))
