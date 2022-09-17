07e2c053782b3047ccbd3206643c660569c84119
Efren511 <luisefrengarciadeleon@gmail.com>
2022-09-17 15:02:58 -0500

    Script to work with long numbers

---
+++ Long_Numbers.py
@@ -0,0 +1,6 @@
+# This program onli works in Python 2.X.X
+
+# We create a long variable
+Number = 511L
+# We wath the data type of the variable
+print("The data type of the Number is: ", type(Number))
